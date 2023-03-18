d6984a1522bc2ab8f7b64bc6869536a70551565a
EdisonBobadillaChancafe <josedison1395@gmail.com>
2023-03-17 23:21:03 -0500

    TSC with inquier testing

--- basesnodes/app.ts
+++ basesnodes/app.ts
@@ -1,20 +1,92 @@
 
 import { funcionmultiplicar } from './helper/function';
 import * as yargs from 'yargs';
-import { number } from 'yargs';
+import { demandCommand, number, required } from 'yargs';
 import * as colors from 'colors'
+import { mainModule } from 'process';
+import { read } from 'fs';
+import { resolve } from 'path';
+require('colors')
+const inquirer = require('inquirer');
 
-/*funcionmultiplicar(5).then(
-    () => {
-        console.log('Se craeo')
+const menu_opciones: Array<Object> = [
+
+    {
+        value: '1',
+        name: 'Crear Tarea'
+    },
+    {
+        value: '2',
+        name: 'Listar Tarea'
+    },
+    {
+        value: '3',
+        name: 'Listar Tareas Complejas'
+    },
+    {
+        value: '4',
+        name: 'Listar Tareas Pendientes'
+    },
+    {
+        value: '5',
+        name: 'Completar Tareas'
+    },
+    {
+        value: '6',
+        name: 'Borrar Tareas'
+    },
+    {
+        value: '0',
+        name: 'Salir'
+    },
+]
+
+const menuopt: Array<Object> = [
+    {
+        type: 'rawlist',
+        name: 'option',
+        message: 'Que desea',
+        choices: menu_opciones
+    }
+]
+
+
+const Monstrarmenu = async (): Promise<any> => {
+    return new Promise(async resolve => {
+
+        console.log('---------------------------'.green)
+        console.log('Seleccione una opcion'.green)
+        console.log('---------------------------\n'.green)
+        const otp = await inquirer.prompt(menuopt)
+        return { otp }
+
     })
-    .catch(
-        (err) => {
-            console.log(err)
-        }
-    )*/
 
-console.log('Adiosmundo')
+
+}
+const pausa = async (): Promise<void> => {
+    const question = {
+        type: 'input',
+        name: 'enter',
+        message: 'Presione Enter para continuar'
+    }
+    await inquirer.prompt(question)
+}
+
+const main = async () => {
+    let otp = ''
+    do {
+        otp = await Monstrarmenu()
+        console.log("\n", otp)
+        await pausa()
+    }
+    while (otp != 'Salir')
+
+
+}
+
+main()
+
 
 
 
